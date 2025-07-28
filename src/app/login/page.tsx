f94de1891b4b11e27d3e12838f801e65188d577e
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-07-28 15:54:59 +0330

    refactor(loginForm): separate login form to a client component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Metadata } from "next";
-import Link from "next/link";
+
+import LoginForm from "@/components/loginForm";
 
 export const metadata: Metadata = {
   title: "ورود",
@@ -11,52 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function Login() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Card className="min-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl text-center">ورود</CardTitle>
-          <CardDescription>
-            ایمیل یا موبایل خود را وارد کنید تا وارد حساب کاربری خود شوید
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">ایمیل یا موبایل</Label>
-                <Input
-                  id="email"
-                  placeholder="example@example.com  / 09034532987"
-                  required
-                  className="text-left"
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">رمز عبور</Label>
-                  <Link
-                    href="/auth/forgot-password"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    رمز عبور خود را فراموش کرده‌اید؟
-                  </Link>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <Button type="submit" className="w-full">
-                ورود
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm flex items-center justify-center gap-2">
-              <p>حساب کاربری ندارید؟</p>
-              <Link href="/register" className="text-blue-500 hover:underline">
-                ثبت نام
-              </Link>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  return <LoginForm />;
 }
