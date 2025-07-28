f4027d406de95e9ccf1c0ad0691cb36349319ac8
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-07-28 15:35:44 +0330

    feat(register): add password fields and validation to registration form, improve focus handling

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -20,21 +20,29 @@ import { useRouter } from "next/navigation";
 
 const imageFormats = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
 
-export const schema = z.object({
-  name: z.string().min(3, { message: "نام باید حداقل 3 کاراکتر باشد" }),
-  lastname: z.string().min(3, { message: "نام خانوادگی باید حداقل 3 کاراکتر باشد" }),
-  email: z.email({ message: "ایمیل معتبر نیست" }),
-  phone: z.string().min(11, { message: "شماره موبایل باید حداقل 11 رقم باشد" }),
-  birthdate: z.date(),
-  nationalCode: z.string().min(3, { message: "کد ملی باید حداقل 3 کاراکتر باشد" }),
-  nationalCard: z
-    .custom<File>(v => v instanceof File, { message: "بارگذاری تصویر کارت ملی الزامی است" })
-    .refine(file => imageFormats.includes(file.type), "فرمت تصویر باید jpeg/png/webp باشد")
-    .refine(file => file.size <= 2 * 1024 * 1024, "حجم فایل حداکثر باید ۲ مگابایت باشد"),
-});
-
-export default function RegisterForm({ email, phone }: { email: string; phone: string }) {
+export default function RegisterForm({ email = "", phone = "" }: { email: string; phone: string }) {
   const router = useRouter();
+
+  const schema = z
+    .object({
+      name: z.string().min(3, { message: "نام باید حداقل 3 کاراکتر باشد" }),
+      lastname: z.string().min(3, { message: "نام خانوادگی باید حداقل 3 کاراکتر باشد" }),
+      email: z.email({ message: "ایمیل معتبر نیست" }),
+      phone: z.string().min(11, { message: "شماره موبایل باید حداقل 11 رقم باشد" }),
+      birthdate: z.date(),
+      nationalCode: z.string().min(3, { message: "کد ملی باید حداقل 3 کاراکتر باشد" }),
+      password: z.string().min(8, { message: "رمز عبور باید حداقل 8 کاراکتر باشد" }),
+      passwordConfirm: z.string().min(8, { message: "تایید رمز عبور باید حداقل 8 کاراکتر باشد" }),
+      nationalCard: z
+        .custom<File>(v => v instanceof File, { message: "بارگذاری تصویر کارت ملی الزامی است" })
+        .refine(file => imageFormats.includes(file.type), "فرمت تصویر باید jpeg/png/webp باشد")
+        .refine(file => file.size <= 2 * 1024 * 1024, "حجم فایل حداکثر باید ۲ مگابایت باشد"),
+    })
+    .refine(data => data.password === data.passwordConfirm, {
+      message: "رمز عبور و تایید رمز عبور مطابقت ندارند",
+      path: ["passwordConfirm"],
+    });
+
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: {
@@ -45,6 +53,8 @@ export default function RegisterForm({ email, phone }: { email: string; phone: s
       nationalCard: undefined,
       email,
       phone,
+      password: "",
+      passwordConfirm: "",
     },
     mode: "onChange",
   });
@@ -82,6 +92,18 @@ export default function RegisterForm({ email, phone }: { email: string; phone: s
       type: "text",
     },
     {
+      name: "password",
+      label: "رمز عبور",
+      placeholder: "رمز عبور",
+      type: "password",
+    },
+    {
+      name: "passwordConfirm",
+      label: "تایید رمز عبور",
+      placeholder: "تایید رمز عبور",
+      type: "password",
+    },
+    {
       name: "email",
       label: "ایمیل",
       placeholder: "example@example.com",
@@ -105,52 +127,55 @@ export default function RegisterForm({ email, phone }: { email: string; phone: s
       placeholder: "0039902211",
       type: "text",
     },
+
     {
       name: "nationalCard",
       label: "کارت ملی",
       placeholder: "کارت ملی",
       type: "file",
+      col: 2,
     },
   ];
 
   return (
-    <Card className="w-full max-w-md p-4">
+    <Card className="w-full max-w-md p-3 gap-2">
       <CardHeader>
         <CardTitle className="text-2xl font-bold text-center">ثبت نام</CardTitle>
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
-            {fields.map(({ name, label, placeholder, type }) => (
-              <FormField
-                key={name}
-                name={name}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{label}</FormLabel>
-                    <FormControl>
-                      {type === "file" ? (
-                        <Input
-                          type="file"
-                          accept={imageFormats.join(",")}
-                          onChange={e => field.onChange(e.target.files?.[0])}
-                          onBlur={field.onBlur}
-                          name={field.name}
-                          ref={field.ref}
-                        />
-                      ) : type === "date" ? (
-                        <DatePicker field={field} />
-                      ) : (
-                        <Input placeholder={placeholder} {...field} type={type} />
-                      )}
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            ))}
-
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid  xs:grid-cols-1  gap-2">
+              {fields.map(({ name, label, placeholder, type, col }) => (
+                <FormField
+                  key={name}
+                  name={name}
+                  render={({ field }) => (
+                    <FormItem className={col ? "col-span-2" : ""}>
+                      <FormLabel>{label}</FormLabel>
+                      <FormControl>
+                        {type === "file" ? (
+                          <Input
+                            type="file"
+                            accept={imageFormats.join(",")}
+                            onChange={e => field.onChange(e.target.files?.[0])}
+                            onBlur={field.onBlur}
+                            name={field.name}
+                            ref={field.ref}
+                          />
+                        ) : type === "date" ? (
+                          <DatePicker field={field} />
+                        ) : (
+                          <Input placeholder={placeholder} {...field} type={type} />
+                        )}
+                      </FormControl>
+                      <FormDescription />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              ))}
+            </div>
             <Button type="submit" className="w-full ">
               ثبت نام
             </Button>
