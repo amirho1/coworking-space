4be6209c2156e7185dd52243d2762ef58529a198
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-07-28 15:55:48 +0330

    feat(login): implement login function with simulated delay and response handling

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -21,3 +21,17 @@ export async function otp(
 
   return { error: "Failed to send OTP", success: false, datetime: undefined };
 }
+
+export async function login(state: { error: string | null; success: boolean }, formData: FormData) {
+  const data = formData.entries();
+
+  // TODO: remove bellow two line for integration
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return { error: null, success: true, datetime: Date.now() };
+
+  const res = await axiosInstance.post(apiRoutes.login, data);
+
+  if (res.status === 200) {
+    return { error: null, success: true };
+  }
+}
