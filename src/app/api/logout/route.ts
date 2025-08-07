fdeb8729900c617ebe0f103731ab4ecdcb920a82
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-08-07 15:38:44 +0330

    refactor(logout): simplify cookie deletion by using deleteAuthCookies utility

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,19 +1,12 @@
 "use server";
 
-import { config } from "@/lib/config";
+import { deleteAuthCookies } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export async function POST() {
   const response = NextResponse.json({ message: "Logged out" });
 
-  response.cookies.set({
-    name: "Authorization",
-    value: "",
-    path: "/",
-    expires: new Date(0), // Forces immediate expiry
-    httpOnly: true,
-    domain: config.env === "production" ? (config.frontendUrl || "").split("://")[1] : undefined,
-  });
+  deleteAuthCookies(response);
 
   return response;
 }
