217cdd3c50ab99f1b4be587eacc30748b27c0e37
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-08-15 15:54:51 +0330

    feat(meeting-room): enhance page function to handle search parameters and pass reserves to MeetingRoomsPage

--- src/app/dashboard/meeting-rooms/[id]/page.tsx
+++ src/app/dashboard/meeting-rooms/[id]/page.tsx
@@ -1,16 +1,35 @@
 import MeetingRoomsPage from "@/components/meeting-rooms";
+import { Reserve } from "@/types";
 import { Metadata } from "next";
+import { getReserves } from "./getReserves";
+import { validDate } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "تقویم اتاق جلسات",
 };
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function page({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] };
+}) {
   const id = (await params).id;
+  const searchParamDate = (await searchParams).date;
+
+  const date = validDate(
+    Array.isArray(searchParamDate) ? "" : searchParamDate ? searchParamDate : ""
+  );
+
+  if (!isNaN(+id)) metadata.title = `تقویم اتاق جلسات  ${id}`;
+
+  const reserves: Reserve[] = await getReserves(date, id);
+
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">تقویم اتاق جلسه {id}</h1>
-      <MeetingRoomsPage />
+      <h1 className="text-2xl font-bold mb-4">تقویم اتاق جلسه {id} </h1>
+      <MeetingRoomsPage reserves={reserves} selectedDate={date} />
     </div>
   );
 }
