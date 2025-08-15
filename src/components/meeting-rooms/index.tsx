2fbb5e13a8863c85295829dd7b9c35b5318f33f3
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-08-15 15:54:36 +0330

    feat(meeting-room): refactor MeetingRoomPage to use selectedDate prop and update routing on date change

--- src/components/meeting-rooms/index.tsx
+++ src/components/meeting-rooms/index.tsx
@@ -1,29 +1,38 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Calendar } from "../ui/Calendar";
 import WeekCalendar from "./WeekCalendar";
 import { Button } from "../ui/button";
+import { Reserve } from "@/types";
+import { useRouter } from "next/navigation";
+import { routes } from "@/lib/utils";
+import { format } from "date-fns";
 
-const today = new Date();
+interface MeetingRoomProps extends React.HTMLAttributes<HTMLDivElement> {
+  reserves: Reserve[];
+  selectedDate: Date;
+}
 
-export default function Index() {
-  const [currentDate, setCurrentDate] = useState(today);
+export default function MeetingRoomPage({ reserves, selectedDate }: MeetingRoomProps) {
+  const [currentDate, setCurrentDate] = useState(selectedDate);
+  const router = useRouter();
+
   const handleDateChange = (date: Date) => {
     setCurrentDate(date);
   };
 
+  useEffect(() => {
+    router.push(routes.meetingRoom(1, format(currentDate, "yyyy-MM-dd")));
+  }, [currentDate]);
+
   return (
     <div className="flex">
       <div className="border-l w-[350px] p-4">
-        <Calendar
-          mode="single"
-          selected={today}
-          onDayClick={handleDateChange}
-        />
+        <Calendar mode="single" selected={currentDate} onDayClick={handleDateChange} />
 
         <Button className="w-full">رزرو اتاق جلسه</Button>
       </div>
-      <WeekCalendar currentDate={currentDate} />
+      <WeekCalendar currentDate={currentDate} reserves={reserves} />
     </div>
   );
 }
